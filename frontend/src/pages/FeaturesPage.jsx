d3f025c8eb8e1757541065bcc4d613e9542a7783
rcabral85 <78968791+rcabral85@users.noreply.github.com>
2025-11-01 15:43:02 -0400

    Add Features simple page component

---
+++ frontend/src/pages/FeaturesPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function FeaturesPage(){
+  return (
+    <div className="page">
+      <h1>Features</h1>
+      <ul>
+        <li>Inventory & classification</li>
+        <li>Mapping & GIS</li>
+        <li>NFPA 291 testing</li>
+        <li>Maintenance & inspections</li>
+        <li>Reporting</li>
+        <li>Security & admin</li>
+      </ul>
+    </div>
+  );
+}
